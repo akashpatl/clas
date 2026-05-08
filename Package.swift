f5a4d3c3a151a993aef0691c6e917ae2302e2890
Akash Patel <akash@patl.xyz>
2026-05-08 09:48:45 +0200

    chore: scaffold Swift package
    SPM executable target with the KeyboardShortcuts dependency.
    Targets macOS 14+ for MenuBarExtra and @Observable.

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+let package = Package(
+    name: "CLAS",
+    platforms: [.macOS(.v14)],
+    products: [
+        .executable(name: "CLAS", targets: ["CLAS"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/sindresorhus/KeyboardShortcuts", from: "2.0.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "CLAS",
+            dependencies: [
+                .product(name: "KeyboardShortcuts", package: "KeyboardShortcuts"),
+            ],
+            path: "Sources/CLAS"
+        ),
+    ]
+)
